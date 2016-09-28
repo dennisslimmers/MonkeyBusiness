cfed32365d8e14531d4ef0edfae399f338ff9da8
blockswallower <dennisslimmers@live.nl>
2016-09-28 10:53:43 +0200

    Added a HTML template instead of a HttpResponse

--- DjangoProjects/urls.py
+++ DjangoProjects/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 from django.contrib import admin
 from MonkeyBusiness import views
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    url(r'^index/', views.index),
+    url(r'^index/', TemplateView.as_view(template_name='index.html')),
     url(r'^admin/', admin.site.urls),
 ]
