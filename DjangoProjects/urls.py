e29ccbce1adc2c75a237cb09e3986ede3987c3e2
Xander <xanderdijk@hotmail.com>
2016-09-30 12:05:57 +0200

    Added basic page URL's (about,cursus,login,register and test) Linked the newly made URL's in the index page, also every new page has a <h1> saying the page name.

--- DjangoProjects/urls.py
+++ DjangoProjects/urls.py
@@ -5,5 +5,10 @@ from django.views.generic.base import TemplateView
 
 urlpatterns = [
     url(r'^index/', TemplateView.as_view(template_name='index.html')),
+    url(r'^test/', TemplateView.as_view(template_name='test.html')),
+    url(r'^about/', TemplateView.as_view(template_name='about.html')),
+    url(r'^cursus/', TemplateView.as_view(template_name='cursus.html')),
+    url(r'^login/', TemplateView.as_view(template_name='login.html')),
+    url(r'^register/', TemplateView.as_view(template_name='register.html')),
     url(r'^admin/', admin.site.urls),
 ]
