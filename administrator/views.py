565129489cf915ca3d977f8d373f11b1a6d7349b
blockswallower <dennisslimmers@live.nl>
2016-11-07 18:27:38 +0100

    Added feature to change user status

--- administrator/views.py
+++ administrator/views.py
@@ -1,24 +1,30 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.db import connection
 
-#render function
+
 @login_required(login_url="login/")
 def renderAdministrator(request):
     users = User.objects.all()
     return render(request, "administrator.html", {"users": users})
+
 
 @login_required(login_url="login/")
 def renderEditUsers(request):
     users = User.objects.all()
     return render(request, "editusers.html", {"users": users})
 
-def makeUserStaff(request):
-    # for item in request.POST:
-    #     if item != request.POST["c
 
+def makeUserStaff(request):
     post = request.POST.copy()
     del post["csrfmiddlewaretoken"]
 
-    return render(request, "test.html", {"post":post})
+    array_keys = list(post.keys())
+
+    for key in array_keys:
+        with connection.cursor() as cursor:
+            cursor.execute("UPDATE auth_user SET is_staff = 1 WHERE username = '"+ key +"'")
+
+    return render(request, "test.html", {"post": array_keys})
 
