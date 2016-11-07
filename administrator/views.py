f12fcacc45e7b22407c4bb115d98ffcea30caab4
blockswallower <dennisslimmers@live.nl>
2016-11-07 16:11:27 +0100

    Added list of users and list of courses to admin page

--- administrator/views.py
+++ administrator/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 #render function
 @login_required(login_url="login/")
 def renderadministrator(request):
-    return render(request, "administrator.html")
+    users = User.objects.all()
+    return render(request, "administrator.html", {"users": users})
+
