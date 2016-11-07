824aa28638f7b9d2893dd76e1c7d84edff856634
Bas <Bassievdploeg@hotmail.com>
2016-11-07 19:13:03 +0100

    New Style

--- login/forms.py
+++ login/forms.py
@@ -5,6 +5,6 @@ from django import forms
 # If you don't do this you cannot use Bootstrap CSS
 class LoginForm(AuthenticationForm):
     username = forms.CharField(label="Username", max_length=30,
-                               widget=forms.TextInput(attrs={'class': 'form-control', 'name': 'username'}))
+                               widget=forms.TextInput(attrs={'class': 'form-control', 'name': 'username', 'placeholder' : "Username"}))
     password = forms.CharField(label="Password", max_length=30,
-                               widget=forms.TextInput(attrs={'class': 'form-control', 'name': 'password'}))
+                               widget=forms.TextInput(attrs={'class': 'form-control', 'name': 'password', 'placeholder' : "Password"}))
